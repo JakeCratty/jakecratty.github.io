d7f09119f0264909599da6658f0e9d6b3a3e7744
jakecratty <jakeryancratty@gmail.com>
2025-03-26 01:18:22 -0700

    Added particle explosions on tap

--- P5/p5js.js
+++ P5/p5js.js
@@ -1,19 +1,29 @@
-//
+
 var b;
 var ballList = [];
+var particleList = [];
+var time = 0;
 function setup()
 {
     var cnv = createCanvas(windowWidth, windowHeight);
     cnv.style('display', 'block')
     background(0)
-    for(i = 0; i < 20; i++)
+    for(i = 0; i < 25; i++)
         ballList.push(new Ball())
 }
 
 function draw()
 {
+    time++;
     background("#21dbde")
     ballList.forEach(ball => update(ball));
+    particleList.forEach(particle => {
+        particle.show()
+        particle.move()
+    });
+    if(time % 100 == 0){
+        ballList.push(new Ball())
+    }
 }
 
 function update(b)
@@ -27,6 +37,65 @@ function windowResized()
     resizeCanvas(windowWidth, windowHeight); 
 }
 
+function mouseClicked(){
+    let mousePos = createVector(mouseX, mouseY)
+    let ballFound = false;
+    let ball;
+    ballListSortedById = [...ballList].sort((a, b) => b.id - a.id).reverse()
+    for(i = ballListSortedById.length - 1; i >= 0 ; i--){
+        ball = ballListSortedById[i];
+        ballPos = createVector(ball.x, ball.y)
+        if(ballPos.dist(mousePos) <= ball.diameter/2)
+        {
+            ballFound= true;
+            ballIndex = ballList.indexOf(ball)
+            break;
+        }
+    }
+    if(ballFound){
+        ballList.splice(ballIndex, 1)
+        particleEffect(ball)
+    }
+
+}
+
+function particleEffect(ball){
+    for(i = 0; i < 100; i++){
+        particleList.push(new Particle(ball.x, ball.y, ball.color, ball.diameter))
+    }
+}
+
+class Particle{
+    constructor(x, y, color, diameter){
+        let newX = random(-diameter/2.0, diameter/2.0);
+        let newY = random(-diameter/2.0, diameter/2.0);
+        let finalPos = createVector(newX, newY).normalize().mult(random(-diameter/2.0, diameter/2.0))
+        this.x = x + finalPos.x
+        this.y = y + finalPos.y
+        this.color = color;
+        this.diameter = diameter/10.0;
+        this.dir = createVector(this.x, this.y).sub(createVector(x, y)).normalize()
+        this.lifetime = 255
+        this.speed = 5
+        console.log("Creating new particle with diameter: " + this.diameter)
+    }
+    move(){
+        this.lifetime-=3;
+        this.x += this.dir.x * this.speed
+        this.y += this.dir.y * this.speed
+        this.speed *= 0.98
+    }
+    show(){
+        this.color.setAlpha(this.lifetime)
+        let strokeColor = color(0)
+        strokeColor.setAlpha(this.lifetime)
+        stroke(strokeColor)
+        fill(this.color)
+        ellipse(this.x, this.y, this.diameter, this.diameter)
+    }
+}
+
+let ballTotal = 0
 class Ball
 {
 
@@ -44,6 +113,8 @@ class Ball
         let b = random(255)
         console.log(r)
         this.color = color(r,g,b)
+        this.id = ballTotal;
+        ballTotal++;
     }
 
     move()
@@ -69,6 +140,7 @@ class Ball
 
     show()
     {
+        stroke(0)
         fill(this.color)
         ellipse(this.x, this.y, this.diameter, this.diameter)
     }
