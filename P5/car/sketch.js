db2df4975e96350823ed306dc2c0b76bced2319a
RyanOTI <jakeryancratty@gmail.com>
2025-03-28 23:13:02 -0700

    Added working car rotation
    testing loading image in github pages

--- P5/car/sketch.js
+++ P5/car/sketch.js
@@ -1,3 +1,9 @@
+let car;
+
+function preload(){
+    car = loadImage('car.png')
+}
+
 function setup()
 {
     p5.disableFriendlyErrors = true;
@@ -7,37 +13,61 @@ function setup()
     }
     cnv.style('display', 'block')
     background(255)
-    
+    rectMode(CENTER)
 
 }
 
 let shift = 0
 let vectorList = []
 let height = 500
+let amt = 0
 function draw()
 {    
     background(177, 218, 252)
+
+    //generate vertices for main path
     vectorList = []
-    for(i = -25; i < windowWidth + 50; i+=25){
+    for(i = -25; i < windowWidth+25; i+=25){
         let x = i + shift
         fill(0)
         stroke(0)
         strokeWeight(1)
-        let y = noise(x*0.0005)*1000 + 500
-        vectorList.push(createVector(i, y))
+        let y = noise(x*0.002)*200 + 500
+        vectorList.push(createVector(i - (windowWidth/2*0), y))
     }
     shift += 3
     
+    //draw main path
     stroke(112, 89, 70)
     strokeWeight(7)
     fill(173, 125, 85)
-    beginShape()
-        for(i = 0; i < vectorList.length; i++){
-            vertex(vectorList[i].x, vectorList[i].y)
+    beginShape(TESS)
+        for(let v of vectorList){
+            vertex(v.x, v.y)
         }
         vertex(windowWidth, windowHeight)
         vertex(0, windowHeight)
-    endShape()
+        vertex(vectorList[0].x, vectorList[0].y)
+    endShape(CLOSE)
+
+    fill(100, 250, 60)
+    stroke(0)
+    strokeWeight(2)
+
+    //get angle between vert 4 and 5
+    stroke(0)
+    strokeWeight(5)
+    vertA = vectorList[3]
+    vertB = vectorList[6]
+    //line(vertA.x, vertA.y, vertB.x, vertB.y)
+    let angle = atan2(vertB.y-vertA.y, vertB.x-vertA.x)
+
+    push()
+    translate(vectorList[4].x, vectorList[4].y-10)
+    rotate(angle)
+    image(car, 0, 0)
+    pop()
+    amt += 0.02
 }
 
 function windowResized()
