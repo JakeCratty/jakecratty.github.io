f5f7c937e0eb9448e6cffee70ac20e47224aab09
RyanOTI <jakeryancratty@gmail.com>
2025-03-31 00:00:27 -0700

    Added moon and time cycle
    Added a moon
    Added a function for changing the colors based on the time
    Added a function for moving the sun and the moon (WIP)

--- P5/car/sketch.js
+++ P5/car/sketch.js
@@ -1,6 +1,7 @@
 let car;
 let carPos;
-let sun;
+let sun, sunPos, moon, moonPos;
+let cycleRadius;
 let basePath;
 let showAll = true
 function preload(){
@@ -14,6 +15,7 @@ function preload(){
     }
     car = loadImage(basePath + "car/car.png")
     sun = loadImage(basePath + "car/sun.png")
+    moon = loadImage(basePath + "car/moon.png")
 }
 
 let mainRoad, bgRoad1
@@ -30,7 +32,8 @@ function setup()
     imageMode(CENTER)
     car.resize(100, 66)
     sun.resize(150, 150)
-
+    moon.resize(150, 150)
+    cycleRadius = (windowHeight - 100) - windowHeight/2
     bgRoad1 = new Terrain(0.004, 600, 0, 1, color(80,50,40), color(150, 100, 65), 5)
     bgRoad2 = new Terrain(0.002, 400, 200, 2, color(112, 89, 70), color(173, 125, 85), 5)
     mainRoad = new Terrain(0.001, 200, 400, 5, color(100,60,50), color(190, 160, 125), 5)
@@ -46,8 +49,8 @@ function draw()
 {    
     background(177, 218, 252)    
     
-    //draw the sun
-    drawSun()
+    //draw the sun and moon
+    //drawSunAndMoon()
 
     if(showAll){
         bgRoad1.updateTerrain()
@@ -73,6 +76,22 @@ function draw()
     textSize(24)
     noStroke()
     text("FPS: " + frameRate().toFixed(2), 50, 50)
+
+    dayNightCycle();
+}
+
+let timeOfDay = 0;
+let dayNightCycleSpeed = 0.5
+let timeDirection = 1
+function dayNightCycle(){
+    timeOfDay += dayNightCycleSpeed * timeDirection
+    if(timeOfDay > 255 || timeOfDay < 0){
+        timeDirection *= -1
+    }
+    tintColor = lerpColor(color(255, 100, 50, 0), color(0, 0, 50, 150), map(timeOfDay, 0, 255, 0, 1));
+    fill(tintColor)
+    rect(windowWidth/2, windowHeight/2, windowWidth, windowHeight)
+
 }
 
 class Terrain{
@@ -115,6 +134,7 @@ class Terrain{
     draw(){
         stroke(this.strokeColor)
         strokeWeight(2)
+        noStroke()
         fill(this.fillColor)
         beginShape(TESS)
             for(let v of this.vectorList){
@@ -148,12 +168,39 @@ function drawCar(){
     pop()
 }
 
-function drawSun(){
-    push()
-        translate(windowWidth-100, 80)
-        rotate(amt)
-        image(sun, 0, 0)
-    pop()
+
+function drawSunAndMoon(){
+    //equation of a circle
+    //x = r*cos(theta) + h
+    //y = r*sin(theta) + k
+    //where k,h is the center of the circle
+    //and theta is the angle in radians
+    // and r is the radius of the circle
+    //for example: x = 100*cos(theta) + 200, y = 100*sin(theta) + 200
+    //where (200,200) is the center of the circle and 100 is the radius
+    //and theta is the angle in radians
+
+    //applying this to the sun and the moon we get
+    let sunAngle = ((timeOfDay) / (255)) * TWO_PI; // Map timeOfDay to a full circle (0 to TWO_PI)
+
+    // Calculate the angle for the moon (opposite side of the circle)
+    let moonAngle = sunAngle + PI; // Opposite side of the circle (180 degrees apart)
+
+    // Ensure angles stay within 0 to TWO_PI
+    sunAngle = sunAngle % TWO_PI;
+    moonAngle = moonAngle % TWO_PI;
+
+    // Calculate positions for the sun and moon
+    let sunX = cycleRadius * cos(sunAngle) + windowWidth / 2;
+    let sunY = cycleRadius * sin(sunAngle) + windowHeight / 2;
+    let moonX = cycleRadius * cos(moonAngle) + windowWidth / 2;
+    let moonY = cycleRadius * sin(moonAngle) + windowHeight / 2;
+
+    // Update sun and moon positions
+    sunPos = createVector(sunX, sunY);
+    moonPos = createVector(moonX, moonY);
+    image(sun, sunPos.x, sunPos.y)
+    image(moon, moonPos.x, moonPos.y)
     amt += 0.001
 }
 function windowResized()
